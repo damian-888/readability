9cbd3b1a53f9110f63b7dcf06bc33dcbc214d4e9
damian-888 <damian.w.clark@gmail.com>
2025-10-29 17:36:07 +0300

    Finished

--- main.cpp
+++ main.cpp
@@ -35,11 +35,67 @@ int main () {
   double avg_letters = (letters / words) * 100;
   double avg_sentences = (sentences / words) * 100;
 
-  double index = round(0.0588 * avg_letters - 0.296 * avg_sentences - 15.8);
+  int index = round(0.0588 * avg_letters - 0.296 * avg_sentences - 15.8);
+  //cout << "Index: " << index << '\n';
 
-  //cout << "Avg Words: " << avg_letters << '\n';
-  //cout << "Avg Sentences: " << avg_sentences << '\n';
-  cout << "Index: " << index << '\n';
+  switch (index) {
+    case 1:
+      cout << "Grade 1\n";
+      break;
+    case 2:
+      cout << "Grade 2\n";
+      break;
+    case 3:
+      cout << "Grade 3\n";
+      break;
+    case 4:
+      cout << "Grade 4\n";
+      break;
+    case 5:
+      cout << "Grade 5\n";
+      break;
+    case 6:
+      cout << "Grade 6\n";
+      break;
+    case 7:
+      cout << "Grade 7\n";
+      break;
+    case 8:
+      cout << "Grade 8\n";
+      break;
+    case 9:
+      cout << "Grade 9\n";
+      break;
+    case 10:
+      cout << "Grade 10\n";
+      break;
+    case 11:
+      cout << "Grade 11\n";
+      break;
+    case 12:
+      cout << "Grade 12\n";
+      break;
+    case 13:
+      cout << "Grade 13\n";
+      break;
+    case 14:
+      cout << "Grade 14\n";
+      break;
+    case 15:
+      cout << "Grade 15\n";
+      break;
+    case 16:
+      cout << "Grade 16\n";
+      break;
+    default:
+      if(index <= 1) {
+        cout << "Before Grade 1\n";
+      } else if(index >= 16) {
+        cout << "Grade 16+\n";
+      } else {
+        return 1;
+      }
+  }
 
 return 0;
 }
@@ -73,7 +129,7 @@ int count_sentences(string input) {
   int sentence_count = 0;
 
   for(int i = 0; i < input.length(); i++) {
-    if(input[i] == '.' || input[i] == '!') {
+    if(input[i] == '.' || input[i] == '!' || input[i] == '?') {
       sentence_count++;
     }
   }
